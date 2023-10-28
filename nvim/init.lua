1fffe111dea8aa9b1f38d9b12df1403dabdd270f
geek-dude <ks.kokkiligadda@gmail.com>
2023-10-29 00:20:56 +0530

    Enabled plugin update notifications

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,15 @@
 require ("keymaps")
 require ("options")
-require ("lazy").setup('plugins')
+require ("lazy").setup('plugins',
+    {
+        checker = 
+        {
+            -- automatically check for plugin updates
+            enabled = true,
+            concurrency = 1, ---@type number? set to 1 to check for updates very slowly
+            notify = true, -- get a notification when new updates are found
+            frequency = 43200, -- check for updates every 12 hours
+            check_pinned = true, -- check for pinned packages that can't be updated
+        },
+    }
+)
