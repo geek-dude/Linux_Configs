decf555d221772fe54267c2738e20087a3217b62
geek-dude <ks.kokkiligadda@gmail.com>
2026-06-08 15:34:51 +0530

    Snacks and file picker displays hidden and gitignored files

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,6 +5,18 @@ return {
       scroll = {
         enabled = false,
       },
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
     },
   },
 }
